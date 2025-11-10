96223e11518f5ce160a8b3109206bd59fdd413e8
mpadge <mark.padgham@email.com>
2025-11-10 12:23:00 +0100

    update mirror-bioc to return actual pkg gh urls
    previous version copied from r-univ just returned the github.com/bioc mirrored versions,
    not the active repo addresses.

--- mirror-bioc.R
+++ mirror-bioc.R
@@ -9,9 +9,48 @@ metabioc_dummy_registry <- function () {
     bioc <- jsonlite::read_json (sprintf ('https://bioconductor.org/packages/json/%s/bioc/packages.json', bioc_version))
     stopifnot (length (bioc) > 2100)
     bioc <- Filter (function (x) !identical (x$PackageStatus, 'Deprecated') || identical (x$Package, 'zlibbioc'), bioc)
-    lapply (setdiff (names (bioc), skiplist), function (x) {
+    out <- lapply (setdiff (names (bioc), skiplist), function (x) {
         baseurl <- ifelse (x %in% nomirror, "https://git.bioconductor.org/packages/", "https://github.com/bioc/")
-        list (package = x, url = paste0 (baseurl, x ))
+        ghurls <- grep ("^https\\:\\/\\/github\\.com\\/", bioc[[x]], value = TRUE)
+        if (length (ghurls) == 0L) {
+            pkgurl <- paste0 (baseurl, x)
+        } else {
+            ghurls <- unlist (strsplit (ghurls, ",\\s*"))
+            ghurls <- unique (gsub ("(\\/(issues|wiki).*|\\/|\\s.*)$", "", tolower (ghurls)))
+            ghcom_url <- grep ("github\\.com", ghurls, value = TRUE)
+            ghio_url <- grep ("github\\.io", ghurls, value = TRUE)
+            if (length (ghcom_url) > 1L) {
+                # Potentially multiple forks: resolve to latest
+                gh_sp <- strsplit (ghcom_url, "\\/")
+                org_repo <- lapply (gh_sp, function (i) tail (i, 2L))
+                org_repo <- do.call (rbind, org_repo)
+                commit_dates <- apply (org_repo, 1, function (i) {
+                    x <- tryCatch (
+                        gh::gh (
+                            "/repos/:owner/:repo/commits",
+                            owner = i [1],
+                            repo = i [2],
+                            .limit = 1L
+                        ) [[1]],
+                        error = function (e) NULL
+                    )
+                    ifelse (is.null (x), NA_character_, x$commit$committer$date)
+                })
+                i <- which.max (as.POSIXct (commit_dates))
+                ghcom_url <- ghcom_url [i]
+            }
+            if (length (ghcom_url) == 1L) {
+                pkgurl <- ghcom_url
+            } else if (length (ghio_url) == 1L) {
+                gh_org <- gsub ("^https\\:\\/\\/|\\..*$", "", ghio_url)
+                gh_repo <- gsub ("^.*\\/", "", ghio_url)
+                pkgurl <- paste0 ("https://github.com/", gh_org, "/", gh_repo)
+            } else {
+                stop ("More than 1 gh url for ", x)
+            }
+            pkgurl <- ghurls [1]
+        }
+        list (package = x, url = pkgurl)
     })
 }
 pkgs <- metabioc_dummy_registry ()
